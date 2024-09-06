018693e6c86803433c403f376b8067c9baf5b888
Tiago Pereira <tiagopereiralopesdossantos@gmail.com>
2024-09-06 13:02:28 -0300

    Use seletores

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react'
+import { Provider } from 'react-redux'
 import Header from './components/Header'
 import Produtos from './containers/Produtos'
 
 import { GlobalStyle } from './styles'
+import { store } from './store'
 
 export type Game = {
     id: number
@@ -16,7 +18,6 @@ export type Game = {
 
 function App() {
     const [games, setGames] = useState<Game[]>([])
-    const [carrinho, setCarrinho] = useState<Game[]>([])
 
     useEffect(() => {
         fetch('http://localhost:4000/produtos')
@@ -24,17 +25,21 @@ function App() {
             .then((res) => setGames(res))
     }, [])
 
+    function adicionarAoCarrinho() {
+        console.log('teste')
+    }
+
     return (
-        <>
+        <Provider store={store}>
             <GlobalStyle />
             <div className="container">
-                <Header itensNoCarrinho={carrinho} />
+                <Header />
                 <Produtos
                     jogos={games}
                     adicionarAoCarrinho={adicionarAoCarrinho}
                 />
             </div>
-        </>
+        </Provider>
     )
 }
 
